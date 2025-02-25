07130608ad27161a579a9afd475afd54271775c0
Success Kingsley <successxodev@gmail.com>
2025-02-24 19:32:53 -0500

    refactor: Rename admin to user to avoid conflicts

--- app/api/v1/admin/router.py
+++ app/api/v1/admin/router.py
@@ -2,10 +2,10 @@
 
 from fastapi import APIRouter
 
-from app.api.v1.admin.admin import router as admin_router
 from app.api.v1.admin.audit import router as audit_router
+from app.api.v1.admin.users import router as users_router
 
 router = APIRouter(tags=["admin"])
 
-router.include_router(admin_router)
+router.include_router(users_router)
 router.include_router(audit_router)
